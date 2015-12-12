4ed1a8caabacb519d21be39b53f20c7f55b503ea
captiosus <royxu652@yahoo.com>
2015-12-12 11:32:16 -0500

    initial routes

--- app.py
+++ app.py
@@ -2,11 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-  return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        return render_template("index.html")
+    else:
+        if session.get('user') == None:
+            return render_template("login.html")
+        else:
+            return render_template("index.html")
+    return render_template("login.html")
 
+@app.route("/community/<community>", methods=["GET", "POST"]))
+def communities(community):
+    if request.method == "POST":
+        code = request.form["code"]
+        return render_template("community.html")
+    return render_template("community.html")
 
+@app.route("/user/<user>")
+def user(user):
+    return render_template("user.html")
+
+@app.route("/about")
+def about():
+    return render_template("/about.html")
+
+@app.route("/creategroup")
+def creategroup():
+    return render_template("/creategroup.html")
 
 if __name__ == "__main__":
     app.debug = True
