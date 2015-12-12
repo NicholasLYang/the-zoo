f1df3967ab9878591f3b8a50c2716d2b909e331b
Nicholas Yang <nick@nicholasyang.com>
2015-12-12 11:52:23 -0500

    stuff

--- mongodb.py
+++ mongodb.py
@@ -2,3 +2,23 @@ from pymongo import MongoClient
 
 connection = MongoClient()
 db = connection["the-zoo"]
+users = db['users']
+
+# Everything should be escaped/hashed by now
+def addUser(uname, pword):
+    db.users.insert(
+        {
+        "username": uname,
+        "password": pword
+        })
+    
+
+def findUser(uname):
+    result = db.users.find_one({"username": uname})
+    return result['username']
+
+# 0 is good, 1 is wrong password, 2 is user does not exist
+def authenticate(uname, pword):
+    user = findUser(uname)
+    if (user['password'] == pword):
+        return 0
